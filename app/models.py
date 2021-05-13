f9ecf9e0617259fe29a0ea0ec9838997df896317
Petri Loukasmäki <petri.loukasmaki@gmail.com>
2021-05-13 21:46:24 +0200

    User model unit tests

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 
 @login_manager.user_loader
@@ -13,12 +13,60 @@ orders_items = db.Table('orders_items',
     db.Column('item_id', db.Integer, db.ForeignKey('item.id'), primary_key=True)
 )
 
+class Permission:
+    SCHEDULE = 1
+    COMMENT = 2
+    REGISTRATION = 4
+    TOURNAMENT = 8
+    ADMIN = 16
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     user = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self,perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
+    @staticmethod
+    def inser_roles():
+        roles = {
+            'User' : [Permission.SCHEDULE, Permission.COMMENT],
+            'Staff' : [Permission.SCHEDULE, Permission.COMMENT, Permission.REGISTRATION],
+            'TournamentManager' : [Permission.SCHEDULE, Permission.COMMENT, Permission.TOURNAMENT],
+            'Admin' : [Permission.SCHEDULE, Permission.COMMENT, Permission.TOURNAMENT, Permission.REGISTRATION]
+        }
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -40,6 +88,14 @@ class User(UserMixin, db.Model):
     
     #order = db.relationship('Order', backref='user')
 
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['EVENT_ADMIN']:
+                self.role = Role.query.filter_by(name='Administrator').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+    
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -108,18 +164,23 @@ class User(UserMixin, db.Model):
         return True
 
 
-        
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
 
-
-
-
-        
-
-
-        
-
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+    
     def __repr__(self):
         return '<User %r>' % self.username
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+    
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
 
 class Event(db.Model):
     __tablename__ = 'event'
