fd1327a7e3324b3dfddecaa22a28ebb42a33c489
Petri Loukasmäki <petri.loukasmaki@gmail.com>
2021-05-13 13:42:50 +0200

    User confirmation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, RegExp, EqualTo
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
@@ -9,8 +9,8 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Log In')
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Lenght(1, 64), Email()])
-    username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp('[A-Za-z][A-Za-z0-9_.]*$', o, 'Usernames must have only letters, numbers, fots or underscores')])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp('[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, numbers, fots or underscores')])
     password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register')
--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from werkzeug.security import generate_password_hash,  check_password_hash
 from flask_login import UserMixin
 from . import db, login_manager
@@ -32,6 +34,7 @@ class User(UserMixin, db.Model):
     #nextofkin = db.Column(db.String(64), nullable=True)
     #nextofkinphoneemail = db.Column(db.String(64), nullable=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
     
     #order = db.relationship('Order', backref='user')
 
@@ -45,6 +48,23 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+        
 
 
         
