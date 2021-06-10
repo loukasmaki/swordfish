e95fd038cafa228d44fac927ea3dd379e7fee303
Petri Loukasmäki <petri.loukasmaki@gmail.com>
2021-06-10 12:11:57 +0200

    stripe integration

--- app/static/js/script-no-ajax.js
+++ app/static/js/script-no-ajax.js
@@ -3,6 +3,7 @@ var stripe = Stripe(checkout_public_key)
 const button = document.querySelector('#buy_now_btn');
 
 button.addEventListener('click', event => {
+    console.log("Pressed checkout")
     stripe.redirectToCheckout({
         sessionId: checkout_session_id
     }).then(function (result) {
